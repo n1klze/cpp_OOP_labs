052a15e1b5a44e09315f0b4cd9f5a0318c1b6d00
Nikita Melnikov <114857730+n1klze@users.noreply.github.com>
2022-12-12 15:18:32 +0700

    Update BigInt.h
    Add a b_int namespace and virteal destructor

--- lab1/BigInt.h
+++ lab1/BigInt.h
@@ -5,113 +5,140 @@
 #include <stdexcept>
 #include <algorithm>
 
-class BigInt {
-private:
-    std::string number;    // string representation of this big integer in base=10
-    bool sign;             //true is negative
+namespace b_int {
+    class BigInt;
 
-    void setNumber(const std::string &str, int radix = 10);
+    BigInt operator+(const BigInt &, const BigInt &);
 
-    void checkStringIsNumeric(const std::string &str);
+    BigInt operator-(const BigInt &, const BigInt &);
 
-    void setSign(bool s);
+    BigInt operator*(const BigInt &, const BigInt &);
 
-    void fixNegativeZero();
+    BigInt operator/(const BigInt &, const BigInt &);
 
-    static std::string add(const std::string &, const std::string &);
+    BigInt operator^(const BigInt &, const BigInt &);
 
-    static std::string padLeft(const std::string &, size_t);
+    BigInt operator%(const BigInt &, const BigInt &);
 
-    static std::string subtract(const std::string &, const std::string &);
+    BigInt operator&(const BigInt &, const BigInt &);
 
-    static std::string removeLeadingZeros(const std::string &);
+    BigInt operator|(const BigInt &, const BigInt &);
 
-    static std::string multiply(const std::string &, const std::string &);
+    std::ostream &operator<<(std::ostream &o, const BigInt &i);
 
-    static std::pair<BigInt, BigInt> divide(const BigInt &, const BigInt &);
+    class BigInt {
+    private:
+        std::string number;    // string representation of this big integer in base=10
+        bool sign;             //true is negative
 
-    std::string toBinString() const;
+        void setNumber(const std::string &str, int radix = DEC);
 
-    static const BigInt ZERO;
-    static const BigInt ONE;
+        void checkStringIsNumeric(const std::string &str);
 
-public:
-    BigInt();
+        void setSign(bool s);
 
-    BigInt(int);
+        void fixNegativeZero();
 
-    BigInt(const std::string &);
+        static std::string add(const std::string &, const std::string &);
 
-    BigInt(const BigInt &);
+        static std::string padLeft(const std::string &, size_t);
 
-    ~BigInt();
+        static std::string subtract(const std::string &, const std::string &);
 
-    BigInt &operator=(const BigInt &);
+        static std::string removeLeadingZeros(const std::string &);
 
-    BigInt operator~() const;
+        static std::string multiply(const std::string &, const std::string &);
 
-    BigInt &operator++();
+        static std::pair<BigInt, BigInt> divide(const BigInt &, const BigInt &);
 
-    const BigInt operator++(int);
+        std::string toBinString() const;
 
-    BigInt &operator--();
+        static const BigInt ZERO;
+        static const BigInt ONE;
 
-    const BigInt operator--(int);
+        enum Radix {
+            BIN = 2,
+            DEC = 10
+        };
 
-    BigInt &operator+=(const BigInt &);
+    public:
+        BigInt();
 
-    BigInt &operator*=(const BigInt &);
+        BigInt(int);
 
-    BigInt &operator-=(const BigInt &);
+        BigInt(const std::string &);
 
-    BigInt &operator/=(const BigInt &);
+        BigInt(const BigInt &);
 
-    BigInt &operator^=(const BigInt &);
+        virtual ~BigInt() = default;
 
-    BigInt &operator%=(const BigInt &);
+        BigInt &operator=(const BigInt &);
 
-    BigInt &operator&=(const BigInt &);
+        BigInt operator~() const;
 
-    BigInt &operator|=(const BigInt &);
+        BigInt &operator++();
 
-    BigInt operator+() const;  // unary +
-    BigInt operator-() const;  // unary -
+        const BigInt operator++(int);
 
-    bool operator==(const BigInt &) const;
+        BigInt &operator--();
 
-    bool operator!=(const BigInt &) const;
+        const BigInt operator--(int);
 
-    bool operator<(const BigInt &) const;
+        BigInt &operator+=(const BigInt &);
 
-    bool operator>(const BigInt &) const;
+        BigInt &operator*=(const BigInt &);
 
-    bool operator<=(const BigInt &) const;
+        BigInt &operator-=(const BigInt &);
 
-    bool operator>=(const BigInt &) const;
+        BigInt &operator/=(const BigInt &);
 
-    explicit operator int() const;
+        BigInt &operator^=(const BigInt &);
 
-    explicit operator std::string() const;
+        BigInt &operator%=(const BigInt &);
 
-    size_t size() const;  // size in bytes
+        BigInt &operator&=(const BigInt &);
 
-    friend BigInt operator+(const BigInt &, const BigInt &);
+        BigInt &operator|=(const BigInt &);
 
-    friend BigInt operator-(const BigInt &, const BigInt &);
+        BigInt operator+() const;  // unary +
+        BigInt operator-() const;  // unary -
 
-    friend BigInt operator*(const BigInt &, const BigInt &);
+        bool operator==(const BigInt &) const;
 
-    friend BigInt operator/(const BigInt &, const BigInt &);
+        bool operator!=(const BigInt &) const;
 
-    friend BigInt operator^(const BigInt &, const BigInt &);
+        bool operator<(const BigInt &) const;
 
-    friend BigInt operator%(const BigInt &, const BigInt &);
+        bool operator>(const BigInt &) const;
 
-    friend BigInt operator&(const BigInt &, const BigInt &);
+        bool operator<=(const BigInt &) const;
 
-    friend BigInt operator|(const BigInt &, const BigInt &);
+        bool operator>=(const BigInt &) const;
 
-    friend std::ostream &operator<<(std::ostream &o, const BigInt &i);
-};
+        explicit operator int() const;
+
+        explicit operator std::string() const;
+
+        size_t size() const;  // size in bytes
+
+        friend BigInt operator+(const BigInt &, const BigInt &);
+
+        friend BigInt operator-(const BigInt &, const BigInt &);
+
+        friend BigInt operator*(const BigInt &, const BigInt &);
+
+        friend BigInt operator/(const BigInt &, const BigInt &);
+
+        friend BigInt operator^(const BigInt &, const BigInt &);
+
+        friend BigInt operator%(const BigInt &, const BigInt &);
+
+        friend BigInt operator&(const BigInt &, const BigInt &);
+
+        friend BigInt operator|(const BigInt &, const BigInt &);
+
+        friend std::ostream &operator<<(std::ostream &o, const BigInt &i);
+    };
+} //namespace b_int
 
 #endif //LAB1_BIGINT_H
